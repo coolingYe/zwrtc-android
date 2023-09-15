73392cde8e0bf0b8c1a96d91cdd9c9eb8893c195
Dio <coolingye@gmail.com>
2023-09-15 19:17:44 +0800

    add webrtc native class.

--- sdk/src/main/java/com/zwrtc/jni/RTCClient.java
+++ sdk/src/main/java/com/zwrtc/jni/RTCClient.java
@@ -1,31 +1,13 @@
 package com.zwrtc.jni;
 
-import static com.zwrtc.jni.Constants.SERVICE_URL;
-
-import android.content.Context;
-
 import com.zwrtc.jni.type.RTCClientConfig;
 
 public abstract class RTCClient {
-    static {
-        System.loadLibrary("zwrtc");
-    }
 
     protected static RTCClientImpl mInstance = null;
 
     public RTCClient() {
 
-    }
-
-    public static synchronized RTCClient create(Context context, String roomId, IRtcEngineEventHandler rtcEngineEventHandler) throws Exception {
-        if (context != null) {
-            RTCClientConfig config = RTCClientConfig.makeJoinConfig(roomId, SERVICE_URL);
-            if (mInstance == null) {
-                mInstance = new RTCClientImpl(config);
-            }
-            return mInstance;
-        }
-        return null;
     }
 
     public static synchronized RTCClient create(RTCClientConfig config) {
